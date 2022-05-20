e2f498eaab5ddf4ea8dbdf5f66bb18a0f5b0be43
Matthieu Gomez <gomez.matthieu@gmail.com>
2022-05-20 09:32:04 -0400

    improve docs

--- src/GroupedArrays.jl
+++ src/GroupedArrays.jl
@@ -10,7 +10,7 @@ GroupedArray{T,N} <: AbstractArray{T,N}
 N-dimensional dense array with elements of type T, where T <: Union{Int, Missing}
 
 GroupedArray has a field ngroups which satisfies the following contract:
-- Right after construction, the set of non missing elements is exactly equal to 1:ngroups.
+- Right after construction, the set of non missing elements in the GroupedArray is equal to 1:ngroups.
 - Afterwards (ie, after using `setindex!`), non missing elements always remain between 1 and ngroups.
 
 """
